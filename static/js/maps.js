d9a477264ebd54cf3bd64ec523877c29cd88e003
zaidAlsharif <zaidalshreef@gmail.com>
2024-05-24 21:40:01 +0300

    Refactor maps.js to improve marker titles and add click event listeners for origin and destination markers.

--- static/js/maps.js
+++ static/js/maps.js
@@ -46,14 +46,4 @@ function initMaps() {
     });
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    loadGoogleMaps();
-});
-
-function loadGoogleMaps() {
-    const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${document.getElementById('google-maps-api-key').value}&callback=initMaps`;
-    script.async = true;
-    script.defer = true;
-    document.head.appendChild(script);
-}
+window.initMaps = initMaps;
