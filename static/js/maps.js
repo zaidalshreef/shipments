0804011c404c25508c2ca51d5ede2f38af255f51
zaidAlsharif <zaidalshreef@gmail.com>
2024-05-24 21:52:55 +0300

    Refactor maps.js to improve marker titles and add click event listeners for origin and destination markers.

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,4 +1,11 @@
-function initMaps() {
+
+let originMap;
+let destinationMap;
+
+async function initMaps() {
+      const { Map } = await google.maps.importLibrary("maps");
+        const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
     const origin = {
         lat: parseFloat(document.getElementById('ship_from_lat').value),
         lng: parseFloat(document.getElementById('ship_from_lng').value)
@@ -12,38 +19,33 @@ function initMaps() {
         zoom: 7,
         center: origin
     };
-    const originMap = new google.maps.Map(document.getElementById('originMap'), mapOptions);
-    const destinationMap = new google.maps.Map(document.getElementById('destinationMap'), mapOptions);
+    const originMap = new Map(document.getElementById('originMap'), mapOptions);
+    const destinationMap = new Map(document.getElementById('destinationMap'), mapOptions);
 
-    const originMarker = new google.maps.marker.AdvancedMarkerElement({
+    const originMarker = new AdvancedMarkerElement({
         position: origin,
         map: originMap,
         title: 'Origin',
         gmpClickable: true
     });
 
-    const destinationMarker = new google.maps.marker.AdvancedMarkerElement({
+    const destinationMarker = new AdvancedMarkerElement({
         position: destination,
         map: destinationMap,
         title: 'Destination',
         gmpClickable: true
     });
 
-    const infoWindow = new google.maps.InfoWindow();
 
     originMarker.addListener('click', ({ domEvent, latLng }) => {
-        infoWindow.close();
-        infoWindow.setContent(originMarker.title);
-        infoWindow.open(originMap, originMarker);
+       console.log('originMarker clicked');
         window.open(`https://www.google.com/maps/search/?api=1&query=${origin.lat},${origin.lng}`, '_blank');
     });
 
     destinationMarker.addListener('click', ({ domEvent, latLng }) => {
-        infoWindow.close();
-        infoWindow.setContent(destinationMarker.title);
-        infoWindow.open(destinationMap, destinationMarker);
+      console.log('destinationMarker clicked');
         window.open(`https://www.google.com/maps/search/?api=1&query=${destination.lat},${destination.lng}`, '_blank');
     });
 }
 
-window.initMaps = initMaps;
+initMaps();
