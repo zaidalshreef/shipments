0f02d27cf462d8bc759bac27abb04f505ed709b9
zaidAlsharif <zaidalshreef@gmail.com>
2024-05-24 18:30:58 +0300

    Refactor maps.js to improve marker titles and add click event listeners for origin and destination markers.

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,34 +1,37 @@
-
-
 function initMaps() {
     const origin = {
-        lat: parseFloat(document.getElementById('ship_from_lat').value),
-        lng: parseFloat(document.getElementById('ship_from_lng').value)
+        lat: parseFloat('{{ ship_from_lat }}'),
+        lng: parseFloat('{{ ship_from_lng }}')
     };
     const destination = {
-        lat: parseFloat(document.getElementById('ship_to_lat').value),
-        lng: parseFloat(document.getElementById('ship_to_lng').value)
+        lat: parseFloat('{{ ship_to_lat }}'),
+        lng: parseFloat('{{ ship_to_lng }}')
     };
 
-    const originMap = new google.maps.Map(document.getElementById('originMap'), {
-        center: origin,
-        zoom: 7
-    });
-
-    const destinationMap = new google.maps.Map(document.getElementById('destinationMap'), {
-        center: destination,
-        zoom: 7
-    });
+    const mapOptions = {
+        zoom: 7,
+        center: origin
+    };
+    const originMap = new google.maps.Map(document.getElementById('originMap'), mapOptions);
+    const destinationMap = new google.maps.Map(document.getElementById('destinationMap'), mapOptions);
 
     const originMarker = new google.maps.Marker({
         position: origin,
         map: originMap,
-        title: 'Ship From Location'
+        title: 'Origin'
     });
-
     const destinationMarker = new google.maps.Marker({
         position: destination,
         map: destinationMap,
-        title: 'Ship To Location'
+        title: 'Destination'
+    });
+
+    originMarker.addListener('click', () => {
+        window.open(`https://www.google.com/maps/search/?api=1&query=${origin.lat},${origin.lng}`, '_blank');
+    });
+
+    destinationMarker.addListener('click', () => {
+        window.open(`https://www.google.com/maps/search/?api=1&query=${destination.lat},${destination.lng}`, '_blank');
     });
 }
+
