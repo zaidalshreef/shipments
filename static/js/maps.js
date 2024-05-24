d7a0bc3743f52f10ece6d2a16dcb7ed6c65d5da4
zaidAlsharif <zaidalshreef@gmail.com>
2024-05-24 19:15:33 +0300

    Refactor maps.js to improve marker titles and add click event listeners for origin and destination markers.

--- static/js/maps.js
+++ static/js/maps.js
@@ -15,26 +15,23 @@ function initMaps() {
     const originMap = new google.maps.Map(document.getElementById('originMap'), mapOptions);
     const destinationMap = new google.maps.Map(document.getElementById('destinationMap'), mapOptions);
 
-    const originMarker = new google.maps.Marker({
+    const originMarker = new google.maps.marker.AdvancedMarkerElement({
         position: origin,
         map: originMap,
         title: 'Origin'
     });
-    const destinationMarker = new google.maps.Marker({
+
+    const destinationMarker = new google.maps.marker.AdvancedMarkerElement({
         position: destination,
         map: destinationMap,
         title: 'Destination'
     });
 
-    console.log('Setting up event listeners for markers');
-
-    originMarker.addListener('click', () => {
-        console.log('Origin marker clicked');
+    originMarker.addEventListener('click', () => {
         window.open(`https://www.google.com/maps/search/?api=1&query=${origin.lat},${origin.lng}`, '_blank');
     });
 
-    destinationMarker.addListener('click', () => {
-        console.log('Destination marker clicked');
+    destinationMarker.addEventListener('click', () => {
         window.open(`https://www.google.com/maps/search/?api=1&query=${destination.lat},${destination.lng}`, '_blank');
     });
 }
