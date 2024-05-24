4819dea23ff23ff5016e7d5573598214fe12ec2b
zaidAlsharif <zaidalshreef@gmail.com>
2024-05-24 22:40:29 +0300

    Refactor maps.js to improve marker titles and add click event listeners for origin and destination markers.

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,12 +1,4 @@
-
-let originMap;
-let destinationMap;
-let destinationMarker;
-let originMarker;
-async function initMaps() {
-      const { Map } = await google.maps.importLibrary("maps");
-        const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
+function initMaps() {
     const origin = {
         lat: parseFloat(document.getElementById('ship_from_lat').value),
         lng: parseFloat(document.getElementById('ship_from_lng').value)
@@ -16,27 +8,37 @@ async function initMaps() {
         lng: parseFloat(document.getElementById('ship_to_lng').value)
     };
 
-    const mapOptions = {
-        zoom: 7,
-        center: origin
-    };
-     originMap = new Map(document.getElementById('originMap'), mapOptions);
-     destinationMap = new Map(document.getElementById('destinationMap'), mapOptions);
+    const originMap = new google.maps.Map(document.getElementById('originMap'), {
+        center: origin,
+        zoom: 12
+    });
 
-     originMarker = new AdvancedMarkerElement({
+    const destinationMap = new google.maps.Map(document.getElementById('destinationMap'), {
+        center: destination,
+        zoom: 12
+    });
+
+    new google.maps.Marker({
         position: origin,
         map: originMap,
-        title: 'Origin',
+        title: 'Ship From Location'
     });
 
-     destinationMarker = new AdvancedMarkerElement({
+    new google.maps.Marker({
         position: destination,
         map: destinationMap,
-        title: 'Destination',
+        title: 'Ship To Location'
     });
-
-
-
 }
 
-initMaps();
+function loadGoogleMaps() {
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyA6mmmEz_JCmb6p-yD6RnDPtRt7o4SXjh8&callback=initMaps`;
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadGoogleMaps();
+});
