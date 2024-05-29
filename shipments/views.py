5b7202ac45e8995d860e22a17a01379e75c054f8
Alhanouf00 <alhanouf.alghayhab@gmail.com>
2024-05-29 23:10:58 +0300

    Add counter to analytic in veiws 6

--- shipments/views.py
+++ shipments/views.py
@@ -30,13 +30,15 @@ def home(request):
     try:
         shipments = Shipment.objects.all()
         shipment_total = shipments.count()
+        
         shipment_delivered = 0 
         shipment_return = 0
+        
     
-        for ship in shipments:
-           if ship.shipping_number  == 'delivered':
+        for ship in shipments :
+           if ship.statuses.last.status  == 'delivered':
             shipment_delivered+1
-           elif ship.statuses.last.status  == 'returned':
+           elif ship.status  == 'returned':
             
             shipment_return+1
         return render(request, 'home.html', {'shipments': shipments ,'shipment_total':shipment_total ,shipment_delivered:'shipment_delivered'
