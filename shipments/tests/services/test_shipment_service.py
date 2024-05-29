e031c31143c68d4639112dbeac8f88e9362823b0
zaidAlsharif <zaidalshreef@gmail.com>
2024-05-30 02:14:04 +0300

    Implemented unit tests for handling shipment creation or update in the shipment service module. Added test cases for creating new shipments, updating existing shipments, and handling return shipments. Also included test cases for error handling and logging. Created fixtures to set up a logs directory for testing purposes.

--- shipments/tests/services/test_shipment_service.py
+++ shipments/tests/services/test_shipment_service.py
@@ -82,7 +82,7 @@ def test_handle_shipment_creation_or_update_existing_shipment(mock_handle_status
         }
     }
     request = rf.post(reverse('shipments:shipment_webhook'), content_type='application/json', data=shipment_data)
-    response = handle_shipment_creation_or_update(shipment_data, 'created', request)
+    response = webhook_handler(request)
     assert response.status_code == 200
     mock_handle_status_update.assert_called_once()
     mock_handle_shipment_update.assert_called_once()
@@ -111,30 +111,9 @@ def test_handle_shipment_creation_or_update_return_shipment(mock_handle_shipment
         'shipping_number': '123456789012',
     }
     request = rf.post(reverse('shipments:shipment_webhook'), content_type='application/json', data=shipment_data)
-    response = handle_shipment_creation_or_update(shipment_data, 'created', request)
-
+    response = webhook_handler(request)
     assert response.status_code == 200
     assert mock_handle_shipment_update.called_once_with(shipment_data)
     assert mock_handle_status_update.called_once_with(123, 'created')
 
 
-@pytest.mark.django_db
-@patch('shipments.services.shipment_service.logger')
-def test_handle_shipment_creation_or_update_error(mock_logger, rf):
-    shipment_data = {
-        'shipment_id': 123,
-        'type': 'new',
-        'merchant': 456,
-        'event': 'test_event',
-        'created_at': '2023-01-01T00:00:00Z',
-        'courier_name': 'Test Courier',
-        'shipping_number': '123456789012',
-    }
-    request = rf.post(reverse('shipments:shipment_webhook'), content_type='application/json', data=shipment_data)
-
-    with patch('shipments.models.Shipment.objects.create') as mock_create:
-        mock_create.side_effect = Exception('Test Exception')
-        response = handle_shipment_creation_or_update(shipment_data, 'created', request)
-
-    assert response.status_code == 500
-    assert 'Error in handle_shipment_creation_or_update' in mock_logger.error.call_args[0][0]
