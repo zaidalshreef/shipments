1ae516d18c307751a405c88bd68a661bd5513b6d
zaidAlsharif <zaidalshreef@gmail.com>
2024-05-30 14:31:11 +0300

    Updated test function to include new mock and modified request data for shipment creation or update in test_shipment_service.

--- shipments/services/salla_service.py
+++ shipments/services/salla_service.py
@@ -152,7 +152,7 @@ def refresh_token(merchant_token):
             token_data = response.json()
             merchant_token.access_token = token_data.get('access_token')
             expires_in = token_data.get('expires')
-            merchant_token.expires_at = datetime.fromtimestamp(expires_in)
+            merchant_token.expires_at = datetime.fromtimestamp(expires_in, timezone.utc)  # Convert to timezone-aware datetime
             merchant_token.save()
             logger.info(f"Token refreshed for merchant id {merchant_token.merchant_id}")
             return True
