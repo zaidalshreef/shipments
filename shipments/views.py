df4118622eba4ff2f94adcd92dd4ad9c1108303e
Alhanouf00 <alhanouf.alghayhab@gmail.com>
2024-06-04 21:12:17 +0300

    Update views in def home 21

--- shipments/views.py
+++ shipments/views.py
@@ -42,7 +42,7 @@ def home(request):
            shipment_delivered+=1
          elif ship.statuses.last().status== 'cancelled':
             shipment_canceled+=1
-            logging.info('cancelled counter $s', shipment_canceled, ship.statuses.last().status)
+            logging.info('cancelled counter %s', shipment_canceled, ship.statuses.last().status)
          elif ship.statuses.last().status== 'returned':
            shipment_returnd+=1
            
