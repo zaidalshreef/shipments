c6624e071ab2f9c62ee67537a6e40386387f9891
Alhanouf00 <alhanouf.alghayhab@gmail.com>
2024-06-05 23:25:15 +0300

    Update in views and home page to add search

--- shipments/views.py
+++ shipments/views.py
@@ -45,12 +45,7 @@ def home(request):
             logging.info(' Shipment cancelled counters %s{shipment_canceled} {ship.statuses.last().status}')
          elif ship.statuses.last().status== 'returned':
            shipment_returnd+=1
-        if request.method == 'GET':
-            shipment_num = request.GET['q']
-            data = Shipment.objects.filter(shipment_num)
-            contex = {
-                'data': data
-            }
+        
  
         return render(request, 'home.html',contex, {'shipments':shipments ,'shipment_total':shipment_total, 'shipment_delivered':shipment_delivered, 'shipment_canceled':shipment_canceled})
     except Exception as e:
